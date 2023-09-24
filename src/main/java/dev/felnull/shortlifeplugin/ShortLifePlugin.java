5454ce0e842e9c5df8bca492984fdeaff5137f90
MORIMORI0317 <morimori.0317@outlook.jp>
2023-09-24 18:02:01 +0900

    Qodanaに追加など

--- src/main/java/dev/felnull/shortlifeplugin/ShortLifePlugin.java
+++ src/main/java/dev/felnull/shortlifeplugin/ShortLifePlugin.java
@@ -1,5 +1,6 @@
 package dev.felnull.shortlifeplugin;
 
+import net.kunmc.lab.ikisugilogger.IkisugiLogger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -11,12 +12,18 @@ public final class ShortLifePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        System.out.println("ｳｧｧ!!ｵﾚﾓｲｯﾁｬｳｩｩｩ!!!ｳｳｳｳｳｳｳｳｳｩｩｩｩｩｩｩｩｳｳｳｳｳｳｳｳ!ｲｨｨｲｨｨｨｲｲｲｨｲｲｲｲ");
+
+        // IKISUGI LOGGER
+        IkisugiLogger logger = new IkisugiLogger("very ikisugi\nshort life");
+        logger.setColorType(IkisugiLogger.ColorType.RAINBOW);
+        logger.setCenter(true);
+        getLogger().info(logger.createLn());
+
+        getLogger().info("ShortLife Plugin has started");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("ShortLife Plugin has stopped");
     }
 }

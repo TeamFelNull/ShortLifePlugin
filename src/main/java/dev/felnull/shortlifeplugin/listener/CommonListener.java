1077240db0b2577f26167d5f10ea32d8d4ad3cb4
くぁーりぃ <kaito6343@gmail.com>
2023-12-04 09:13:53 +0900

    commonlistenerリファクタ

--- src/main/java/dev/felnull/shortlifeplugin/listener/CommonListener.java
+++ src/main/java/dev/felnull/shortlifeplugin/listener/CommonListener.java
@@ -4,6 +4,7 @@ import dev.felnull.shortlifeplugin.SLConfig;
 import dev.felnull.shortlifeplugin.SLPermissions;
 import dev.felnull.shortlifeplugin.ShortLifePlugin;
 import dev.felnull.shortlifeplugin.decoration.BloodExpression;
+import dev.felnull.shortlifeplugin.match.Match;
 import dev.felnull.shortlifeplugin.match.MatchManager;
 import dev.felnull.shortlifeplugin.match.TeamBaseMatch;
 import dev.felnull.shortlifeplugin.utils.WeaponMechanicsUtils;
@@ -63,14 +64,21 @@ public class CommonListener implements Listener {
      */
     @EventHandler(priority = EventPriority.MONITOR)
     public void onJoin(PlayerJoinEvent e) {
-
-        // テストモードの場合、参加時にメッセージを送信
         if (SLConfig.isTestMode()) {
-            Component message = Component.text("ShortLifePluginはテストモードです。");
-            message = message.appendNewline().append(Component.text("このメッセージを見た場合は運営に報告してください！"));
-            message = message.color(NamedTextColor.GREEN);
-            e.getPlayer().sendMessage(message);
+            sendTestMessage(e.getPlayer());
         }
+    }
+
+    /**
+     * プレイヤーにテストモードであるというメッセージを送信
+     *
+     * @param player プレイヤー
+     */
+    private static void sendTestMessage(Player player) {
+        Component message = Component.text("ShortLifePluginはテストモードです。");
+        message = message.appendNewline().append(Component.text("このメッセージを見た場合は運営に報告してください！"));
+        message = message.color(NamedTextColor.GREEN);
+        player.sendMessage(message);
     }
 
     /**
@@ -83,22 +91,31 @@ public class CommonListener implements Listener {
         // WeaponMechanicsのダメージ以外
         if (!(e instanceof WeaponMechanicsEntityDamageByEntityEvent) && e.getEntity() instanceof LivingEntity livingEntity
                 && BloodExpression.isSpawnDamageParticle(livingEntity, e.getDamage())) {
-
-            BloodExpression.getDamageBox(livingEntity, e.getCause()).ifPresent(pointAndIsCritical -> {
-                BoundingBox damageBox;
-                Optional<BoundingBox> criticalDamageBox;
-
-                if (pointAndIsCritical.getRight()) {
-                    damageBox = livingEntity.getBoundingBox();
-                    criticalDamageBox = Optional.of(pointAndIsCritical.getLeft());
-                } else {
-                    damageBox = pointAndIsCritical.getLeft();
-                    criticalDamageBox = Optional.empty();
-                }
-
-                BloodExpression.spawnDamageParticle(livingEntity, damageBox, criticalDamageBox, e.getDamage());
-            });
+            spawnDamageParticle(e, livingEntity);
         }
+    }
+
+    /**
+     * ダメージパーティクルを表示させる
+     *
+     * @param e            イベント
+     * @param livingEntity ダメージ対象
+     */
+    private static void spawnDamageParticle(EntityDamageEvent e, LivingEntity livingEntity) {
+        BloodExpression.getDamageBox(livingEntity, e.getCause()).ifPresent(pointAndIsCritical -> {
+            BoundingBox damageBox;
+            Optional<BoundingBox> criticalDamageBox;
+
+            if (pointAndIsCritical.getRight()) {
+                damageBox = livingEntity.getBoundingBox();
+                criticalDamageBox = Optional.of(pointAndIsCritical.getLeft());
+            } else {
+                damageBox = pointAndIsCritical.getLeft();
+                criticalDamageBox = Optional.empty();
+            }
+
+            BloodExpression.spawnDamageParticle(livingEntity, damageBox, criticalDamageBox, e.getDamage());
+        });
     }
 
     /**
@@ -111,9 +128,20 @@ public class CommonListener implements Listener {
         LivingEntity livingEntity = e.getVictim();
         double damage = e.getFinalDamage();
         if (BloodExpression.isSpawnDamageParticle(livingEntity, damage)) {
-            Optional<BoundingBox> damagePointBox = WeaponMechanicsUtils.getDamagePointBox(livingEntity, e.getPoint());
-            BloodExpression.spawnDamageParticle(livingEntity, livingEntity.getBoundingBox(), damagePointBox, damage);
+            spawnDamageParticle(e, livingEntity, damage);
         }
+    }
+
+    /**
+     * 武器によるダメージパーティクルを表示させる
+     *
+     * @param e            イベント
+     * @param livingEntity エンティティ
+     * @param damage       ダメージ
+     */
+    private static void spawnDamageParticle(WeaponDamageEntityEvent e, LivingEntity livingEntity, double damage) {
+        Optional<BoundingBox> damagePointBox = WeaponMechanicsUtils.getDamagePointBox(livingEntity, e.getPoint());
+        BloodExpression.spawnDamageParticle(livingEntity, livingEntity.getBoundingBox(), damagePointBox, damage);
     }
 
     /**
@@ -125,19 +153,32 @@ public class CommonListener implements Listener {
     public void onPlayerDeath(PlayerDeathEvent e) {
         Player killed = e.getEntity();
         Player killer = e.getEntity().getKiller();
-        if (killer != null) {
-            ItemStack stack = killer.getEquipment().getItemInMainHand();
-            Component weapon = !stack.isEmpty() ? stack.displayName() : Component.text("[素手]").color(NamedTextColor.RED).decorate(TextDecoration.BOLD);
-            NamedTextColor killedColor = TeamBaseMatch.getTeamColor(killed);
-            NamedTextColor killerColor = TeamBaseMatch.getTeamColor(killer);
-
-            e.deathMessage(null);
-            e.getPlayer().getWorld().sendMessage(Component
-                    .text(alignName(killed.getName())).color(killedColor)
-                    .append(Component.text(" <-Killed-- ").color(NamedTextColor.DARK_GRAY).decorate(TextDecoration.BOLD))
-                    .append(Component.text(killer.getName() + " ").color(killerColor))
-                    .append(weapon));
+        if (killer == null) {
+            return;
         }
+
+        sendDeathMessage(e, killed, killer);
+    }
+
+    /**
+     * 死亡メッセージを送信
+     *
+     * @param e      死亡イベント
+     * @param killed 死亡者
+     * @param killer 殺害者
+     */
+    private void sendDeathMessage(PlayerDeathEvent e, Player killed, Player killer) {
+        ItemStack stack = killer.getEquipment().getItemInMainHand();
+        Component weapon = !stack.isEmpty() ? stack.displayName() : Component.text("[素手]").color(NamedTextColor.RED).decorate(TextDecoration.BOLD);
+        NamedTextColor killedColor = TeamBaseMatch.getTeamColor(killed);
+        NamedTextColor killerColor = TeamBaseMatch.getTeamColor(killer);
+
+        e.deathMessage(null);
+        e.getPlayer().getWorld().sendMessage(Component
+                .text(alignName(killed.getName())).color(killedColor)
+                .append(Component.text(" <-Killed-- ").color(NamedTextColor.DARK_GRAY).decorate(TextDecoration.BOLD))
+                .append(Component.text(killer.getName() + " ").color(killerColor))
+                .append(weapon));
     }
 
     /**
@@ -162,9 +203,18 @@ public class CommonListener implements Listener {
     public void onPlayerKill(EntityDeathEvent e) {
         Player killer = e.getEntity().getKiller();
         if (killer != null) {
-            killer.playSound(killer, ENTITY_ZOMBIE_ATTACK_WOODEN_DOOR, 0.6f, 0.5f);
-            killer.playSound(killer, ENTITY_EXPERIENCE_ORB_PICKUP, 0.6f, 0.5f);
+            playKillSound(killer);
         }
+    }
+
+    /**
+     * 殺害した際の効果音を鳴らす
+     *
+     * @param killer 殺害者
+     */
+    private static void playKillSound(Player killer) {
+        killer.playSound(killer, ENTITY_ZOMBIE_ATTACK_WOODEN_DOOR, 0.6f, 0.5f);
+        killer.playSound(killer, ENTITY_EXPERIENCE_ORB_PICKUP, 0.6f, 0.5f);
     }
 
     /**
@@ -172,9 +222,16 @@ public class CommonListener implements Listener {
      */
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onServerJoin(PlayerJoinEvent e) {
-        Player joinp = e.getPlayer();
-        joinp.sendMessage(Component.text("ShortLifeへようこそ!").color(NamedTextColor.GOLD).decorate(TextDecoration.BOLD));
+        sendWelcomeMessage(e);
+    }
 
+    /**
+     * サーバー参加メッセージを送る
+     *
+     * @param e 参加イベント
+     */
+    private static void sendWelcomeMessage(PlayerJoinEvent e) {
+        e.getPlayer().sendMessage(Component.text("ShortLifeへようこそ!").color(NamedTextColor.GOLD).decorate(TextDecoration.BOLD));
     }
 
     /**
@@ -189,34 +246,46 @@ public class CommonListener implements Listener {
         World toWorld = player.getWorld();
 
         // 試合中のワールドに、想定外の方法で参加した時の警告表示
-        if (fromWorld != toWorld) {
-            MatchManager matchManager = MatchManager.getInstance();
-            // 移動先のワールドで試合が行われている場合
-            matchManager.getMatchByWorld(toWorld).ifPresent(worldMatch -> {
-                
-                // プレイヤーが未参加または、参加している試合が世界の試合と一致しない場合
-                if (matchManager.getJoinedMatch(player).stream().noneMatch(match -> match == worldMatch)) {
-                    Component message = Component.text("試合中").color(NamedTextColor.RED)
-                            .append(worldMatch.createDisplayComponent().color(NamedTextColor.GOLD))
-                            .append(Component.text("のワールドに、想定外の方法で侵入したようです。"));
-
-                    message = message.appendNewline().append(Component.text("試合に参加していないため、干渉することはできません。"));
-
-                    // 権限がある場合は、参加用メッセージを追加
-                    if (player.hasPermission(SLPermissions.COMMANDS_MATCH.get())) {
-                        Component clickHere = Component.text("[ここをクリック]")
-                                .style(Style.style().color(NamedTextColor.YELLOW).clickEvent(ClickEvent.runCommand("/match join " + worldMatch.getId())).build());
-
-                        message = message.appendNewline().append(Component.text("参加する場合は")
-                                .append(clickHere)
-                                .append(Component.text("してください。")));
-                    }
-
-                    message = message.appendNewline().append(Component.text("故意で侵入していない場合は、不具合として報告してください！"));
-
-                    player.sendMessage(message);
-                }
-            });
+        if (fromWorld == toWorld) {
+            return;
         }
+
+        MatchManager matchManager = MatchManager.getInstance();
+        // 移動先のワールドで試合が行われている場合
+        matchManager.getMatchByWorld(toWorld).ifPresent(worldMatch -> {
+
+            // プレイヤーが未参加または、参加している試合が世界の試合と一致しない場合
+            if (matchManager.getJoinedMatch(player).stream().noneMatch(match -> match == worldMatch)) {
+                sendUnexpectedMatchJoinMessage(player, worldMatch);
+            }
+        });
+    }
+
+    /**
+     * プレイヤーが試合に非正規の方法で参加したメッセージを送る
+     *
+     * @param player     プレイヤー
+     * @param worldMatch 試合
+     */
+    private static void sendUnexpectedMatchJoinMessage(Player player, Match worldMatch) {
+        Component message = Component.text("試合中").color(NamedTextColor.RED)
+                .append(worldMatch.createDisplayComponent().color(NamedTextColor.GOLD))
+                .append(Component.text("のワールドに、想定外の方法で侵入したようです。"));
+
+        message = message.appendNewline().append(Component.text("試合に参加していないため、干渉することはできません。"));
+
+        // 権限がある場合は、参加用メッセージを追加
+        if (player.hasPermission(SLPermissions.COMMANDS_MATCH.get())) {
+            Component clickHere = Component.text("[ここをクリック]")
+                    .style(Style.style().color(NamedTextColor.YELLOW).clickEvent(ClickEvent.runCommand("/match join " + worldMatch.getId())).build());
+
+            message = message.appendNewline().append(Component.text("参加する場合は")
+                    .append(clickHere)
+                    .append(Component.text("してください。")));
+        }
+
+        message = message.appendNewline().append(Component.text("故意で侵入していない場合は、不具合として報告してください！"));
+
+        player.sendMessage(message);
     }
 }
